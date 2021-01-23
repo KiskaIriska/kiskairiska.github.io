e87329140c17725800b4ea97a92f1227cbdada3b
KiskaIriska <elena150620@gmail.com>
2021-01-23 09:31:37 +0400

    js

--- feadback.js
+++ feadback.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", function (event) {
     event.preventDefault();
     var data = new FormData(form);
 
-    if ((data.get("adress") == "") || (data.get("phone") == "")) {
+    if ((data.get("email") == "") || (data.get("login") == "")|| (data.get("textMessage") == "")) {
         Swal.fire("Ошибка!", "пожалуйста заполните все поля", "error");
     } else {
         Swal.fire("Спасибо что написали нам", "Скоро мы вам ответим", "success");
